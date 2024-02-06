5061c6e39a090d113f153d081d933caca451fda3
Wangari-j <wangarijudy@gmail.com>
2024-02-06 22:13:06 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,12 @@
+name = "Judy"
+
+if name == "Jane":
+    print("Akinyi")
+elif name == "Jackie":
+    print("Mwende")
+elif name == "Joy":
+    print("Kendi")
+elif name == "Aisha":
+     print("Adan")
+else:
+    print("Wangari")
